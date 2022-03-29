d5abb64c975c2abb27fa85fba479f43bef4dd9a4
‘Andrei <andrei.dima@usm.ro>
2022-03-29 14:13:38 +0300

    a

--- resources/views/aplicatie_angajati/pontaj/pontaj_verifica.blade.php
+++ resources/views/aplicatie_angajati/pontaj/pontaj_verifica.blade.php
@@ -72,13 +72,15 @@
                     <div class="row input-group custom-search-form justify-content-center">
                         <div class="col-lg-12 text-center">
                             <button class="btn btn-primary text-white mb-2 border border-dark rounded-3 shadow" type="submit"
-                                name="action" value="saptamana_anterioara">
+                                name="action" value="saptamana_anterioara"
+                                style="background-color: #FC4A1A; border:2px solid white;">
                                 << Săptămâna anterioară
                             </button>
                         </div>
                         <div class="col-lg-12 text-center">
                             <button class="btn btn-primary text-white mb-2 border border-dark rounded-3 shadow" type="submit"
-                                name="action" value="saptamana_urmatoare">
+                                name="action" value="saptamana_urmatoare"
+                                style="background-color: #FC4A1A; border:2px solid white;">
                                 Săptămâna următoare >>
                             </button>
                         </div>
