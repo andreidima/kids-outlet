212d1cbbcb9d720fcd6a67d8049d1252fb4f9227
‘Andrei <andrei.dima@usm.ro>
2023-03-10 10:52:24 +0200

    a

--- app/Http/Controllers/AngajatAplicatieController.php
+++ app/Http/Controllers/AngajatAplicatieController.php
@@ -123,7 +123,7 @@ class AngajatAplicatieController extends Controller
         } elseif ($angajat->sectia === "Mostre"){
             $produse = Produs::where('activ', 1)->latest()->get(); // se afiseaza toate
         } else {
-            $produse = Produs::where('id', 0); // se afiseaza colectia goala
+            $produse = Produs::where('activ', 1)->latest()->get(); // se afiseaza toate
         }
 
         // dd($produse->toArray());
