00051a228f104cd0d2ac3a78e81dac90b55cc3f1
‘Andrei <andrei.dima@usm.ro>
2023-12-15 12:22:09 +0200

    a

--- resources/views/aplicatie_angajati/meniul_principal.blade.php
+++ resources/views/aplicatie_angajati/meniul_principal.blade.php
@@ -94,8 +94,8 @@
                         || ($angajat->id === 4) // Mocanu Geanina
                         || ($angajat->id === 16) // Fodoroiu Geta
                         || ($angajat->id === 91) // Borchina Liliana
-                        || ($angajat->id === 156) // Craciun Gabriel
-                        || ($angajat->id === 162) // Toader Maria
+                        // || ($angajat->id === 156) // Craciun Gabriel
+                        // || ($angajat->id === 162) // Toader Maria
                         || ($angajat->id === 231) // Gologus Maricica
                         || ($angajat->id === 234) // Munteanu Genoveva
                     )
